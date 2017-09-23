6f7ae822680ccec255096e480e2f45b031575618
JohnG <jonathanguillou1978@gmail.com>
2017-09-23 12:25:17 +0200

    Modif minim css (no comment)

--- my-app/src/components/pageComponent/main.js
+++ my-app/src/components/pageComponent/main.js
@@ -5,12 +5,8 @@ class Page extends Component {
       return (
 			<main>
 			    <div className="graphics"></div>
-				<div className="blocA">
-					<h1>blocA</h1>
-				</div>
-				<div className="blocB">
-					<h1>blocB</h1>
-				</div>
+{/*				<div className="blocA"><h1>blocA</h1></div>
+				<div className="blocB"><h1>blocB</h1></div>*/}
 			</main>
         );
   	}
