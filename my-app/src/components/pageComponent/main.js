7eac2d5be4b8bfc465ef1911d1eff1eb5e1d0654
Alexandre Douard <kalambik@gmail.com>
2017-09-23 15:12:54 +0200

    css-accueil

--- my-app/src/components/pageComponent/main.js
+++ my-app/src/components/pageComponent/main.js
@@ -4,7 +4,11 @@ class Page extends Component {
    render() {
       return (
 			<main>
-			    <div className="graphics"></div>
+			    <div className="triangle_black_border"></div>
+			    <div className="triangle_red"></div>
+			    <div className="triangle_red_small"></div>
+
+			 
 {/*				<div className="blocA"><h1>blocA</h1></div>
 				<div className="blocB"><h1>blocB</h1></div>*/}
 			</main>
