7a5db83bc35eec4b9abb9e45512b30bb1324254f
Alexandre Douard <kalambik@gmail.com>
2017-09-24 16:35:40 +0200

    alex-formulaire

--- my-app/src/page_content/form.js
+++ my-app/src/page_content/form.js
@@ -4,18 +4,45 @@ import { BrowserRouter , Switch, Route, Link } from 'react-router-dom';
 	
 
 class Form extends Component {
+	constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: data,
+    });
+  }
   render() {
     return (
       <div>
-        <div className="form">
-          <Link to={'/board'}><button>connexion</button></Link>
+        <div className="form_connection">
+            <form onSubmit={this.handleSubmit}>
+             
+              <input id="username"  name="username" placeholder="username" type="text" />
+
+            
+              <input id="email" name="email" placeholder="email" type="email" />
+
+            
+              <input id="password" placeholder="password" name="password" type="text" />
+
+              <Link to={'/board'}><button>connexion</button></Link>
+            </form>
+         
         </div>
         
         <div>
             <div className="triangle_black_border"></div>
             <div className="triangle_red"></div>
             <div className="triangle_red_small"></div>
-          </div>
+        </div>
+
       </div>
     );
   }
