66afbf53af836c60585966a0b972affc81241c9a
alexandre <kalambik@gmail.com>
2017-09-25 09:43:24 +0200

    Auto stash before merge of "master" and "origin/master"

--- my-app/src/components/headerComponent/header.js
+++ my-app/src/components/headerComponent/header.js
@@ -24,7 +24,6 @@ class Header extends Component {
           </div>
         	</header>
           <Switch>
-
               <Route exact={true} path='/' component={Home} />
               <Route path='/inscription' component={Inscription} />
               <Route path='/connection' component={Connexion} />
